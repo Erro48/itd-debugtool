3fafc7bd98b60798c7f14bc777e47a360db7c846
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2023-04-30 15:10:02 +0200

    Minor fix

--- src/components/card.js
+++ src/components/card.js
@@ -10,7 +10,7 @@ const Card = ({ title, description, address, version }) => {
 	}
 
 	return (
-		<a href='/' className='card td-card py-3 px-4'>
+		<a href='/' className='card td-card py-3 px-4 py-sm-2'>
 			<div className='row'>
 				<div className='col-10 col-sm-12'>
 					<header>
@@ -20,7 +20,7 @@ const Card = ({ title, description, address, version }) => {
 						</div>
 						<h4 className='subtitle d-none d-sm-block'>{address}</h4>
 					</header>
-					<div className='description pt-2'>{description}</div>
+					<div className='description pt-2 pt-sm-1'>{description}</div>
 				</div>
 				<div className='col-2 d-flex d-sm-none'>
 					<img
