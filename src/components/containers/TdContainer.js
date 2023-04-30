3c77e33865b1af6aaf88eae89f25fb9e01a8d241
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2023-04-30 11:10:27 +0200

    Add td container basic style

---
+++ src/components/containers/TdContainer.js
@@ -0,0 +1,30 @@
+import Card from '../Card'
+import './tdContainer.css'
+
+const cards = [
+	{ title: 'tractor', description: 'Is a tractor which goes vroom vroom' },
+	{ title: 'field', description: 'Boring land of dirt' },
+	{ title: 'wheet', description: 'Vibing in the wind 😎' },
+]
+
+function TdContainer() {
+	return (
+		<section className='td-container col-12 col-sm-3'>
+			<header className='pt-3 px-2'>
+				<h2>Thing Descriptions</h2>
+				<hr />
+			</header>
+			<ul className='px-2'>
+				{cards.map((card, index) => {
+					return (
+						<li key={index}>
+							<Card title={card.title} description={card.description} />
+						</li>
+					)
+				})}
+			</ul>
+		</section>
+	)
+}
+
+export default TdContainer
