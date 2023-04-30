4f1f137142cabf2bd19ce8f5a4e7f0db2a920110
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2023-04-30 12:15:24 +0200

    Add card separator in cardlist

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import Card from './Card'
 
+import './cardList.css'
+
 const CardList = ({ cards }) => {
+	const insertHr = (aboveCard) => {
+		if (cards.indexOf(aboveCard) != cards.length - 1) {
+			return <hr className='card-separator m-0 d-none d-sm-block' />
+		}
+	}
 	return (
 		<ul className='px-2'>
 			{cards.map((card) => {
 				return (
-					<li key={card.title}>
-						<Card {...card} />
-					</li>
+					<React.Fragment>
+						<li key={card.title}>
+							<Card {...card} />
+						</li>
+						{insertHr(card)}
+					</React.Fragment>
 				)
 			})}
 		</ul>
