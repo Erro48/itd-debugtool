2df5d09a6e1d96a42401087cfbde20e056d0e54a
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2023-05-18 18:58:22 +0200

    Fix missing key in cards list

--- src/components/utils/CardList.js
+++ src/components/utils/CardList.js
@@ -4,21 +4,13 @@ import Card from './Card'
 import './cardList.css'
 
 const CardList = ({ cards, className }) => {
-	const insertHr = (aboveCard) => {
-		if (cards.indexOf(aboveCard) != cards.length - 1) {
-			return <hr className='card-separator m-0 d-none d-sm-block' />
-		}
-	}
 	return (
 		<ul className={'p-2 overflow-auto ' + className}>
 			{cards.map((card) => {
 				return (
-					<React.Fragment>
-						<li key={card.title}>
-							<Card {...card} />
-						</li>
-						{insertHr(card)}
-					</React.Fragment>
+					<li key={card.title}>
+						<Card {...card} />
+					</li>
 				)
 			})}
 		</ul>
