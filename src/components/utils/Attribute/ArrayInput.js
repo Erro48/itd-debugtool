7f100e35c95a9a2011184d37f505946ff0ea9094
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2023-07-09 15:13:43 +0200

    Remove old attributes components

--- src/components/utils/Attribute/ArrayInput.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ArrayInput = () => {
-	return <></>
-}
-
-export default ArrayInput
--- src/components/utils/Attribute/StringInput.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const StringInput = () => {
-	return <input type='text' name='' id='' />
-}
-
-export default StringInput
